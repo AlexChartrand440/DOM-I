2cfdd2e1952281ada59d820140b1f70b1e164ed3
Neytorokx <neytorokx@gmail.com>
2019-12-02 17:12:46 -0600

    Initial push

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const siteContent = {
   "cta": {
     "h1": "DOM Is Awesome",
     "button": "Get Started",
-    "img-src": "img/header-img.png"
+    "imgsrc": "img/header-img.png"
   },
   "main-content": {
     "features-h4":"Features",
@@ -39,4 +39,141 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+const siteData = [];
+
+for (var data in siteContent)
+  siteData.push(siteContent[data]);
+
+function processNav() {
+
+  const navData = [];
+  const navItems = Array.from(Array.from(document.getElementsByTagName('nav'))[0].getElementsByTagName('a'));
+
+  for (var data in siteData[0])
+    navData.push(siteData[0][data]);
+  
+  for (var i = 0; i < navItems.length; i++) {
+    navItems[i].innerHTML = navData[i];
+    navItems[i].style.color = 'green';
+    navItems[i].href = '/' + navData[i] + '.html';
+  }
+
+  { // Building new elements
+
+    var itemOne = document.createElement('a');
+    var itemTwo = document.createElement('a');
+
+    itemOne.innerHTML = 'Github';
+    itemTwo.innerHTML = 'Portolio';
+
+    itemOne.href = 'https://github.com/Neytoro';
+    itemTwo.href = 'neytorokx.com';
+
+    itemOne.style.color = 'green';
+    itemTwo.style.color = 'green';
+
+    Array.from(document.getElementsByTagName('nav'))[0].prepend(itemOne);
+    Array.from(document.getElementsByTagName('nav'))[0].appendChild(itemTwo);
+
+  }
+
+  document.getElementById('logo-img').src = navData[6];
+
+}
+
+processNav();
+
+function processCTA() {
+
+  const ctaData = [];
+
+  for (var data in siteData[1])
+    ctaData.push(siteData[1][data]);
+
+  Array.from(document.getElementsByClassName('cta'))[0].style.justifyContent = 'space-around';
+
+  const CTAH = Array.from(Array.from(document.getElementsByClassName('cta-text'))[0].getElementsByTagName('h1'))[0];
+
+  CTAH.innerHTML = ctaData[0].replace(/ /g, '</br>');
+
+  Array.from(Array.from(document.getElementsByClassName('cta-text'))[0].getElementsByTagName('button'))[0].innerHTML = ctaData[1];
+
+  Array.from(Array.from(document.getElementsByClassName('cta-text'))[0].getElementsByTagName('button'))[0].onclick = () => {
+
+    window.location = '/sign_up.html';
+
+  };
+
+  document.getElementById('cta-img').src = ctaData[2];
+
+}
+
+processCTA();
+
+function processMainContent() {
+
+  const mainData = [];
+
+  for (var data in siteData[2])
+    mainData.push(siteData[2][data]);
+
+  { // Top Sections
+
+    const sectionA = Array.from(Array.from(document.getElementsByClassName('top-content'))[0].getElementsByClassName('text-content'))[0];
+    const sectionB = Array.from(Array.from(document.getElementsByClassName('top-content'))[0].getElementsByClassName('text-content'))[1];
+  
+    Array.from(sectionA.getElementsByTagName('h4'))[0].innerHTML = mainData[0];
+    Array.from(sectionA.getElementsByTagName('p'))[0].innerHTML = mainData[1];
+  
+    Array.from(sectionB.getElementsByTagName('h4'))[0].innerHTML = mainData[2];
+    Array.from(sectionB.getElementsByTagName('p'))[0].innerHTML = mainData[3];
+
+  }
+
+  document.getElementById('middle-img').src = mainData[4];
+
+  { // Bottom Sections
+
+    const sectionC = Array.from(Array.from(document.getElementsByClassName('bottom-content'))[0].getElementsByClassName('text-content'))[0];
+    const sectionD = Array.from(Array.from(document.getElementsByClassName('bottom-content'))[0].getElementsByClassName('text-content'))[1];
+    const sectionE = Array.from(Array.from(document.getElementsByClassName('bottom-content'))[0].getElementsByClassName('text-content'))[2];
+  
+    Array.from(sectionC.getElementsByTagName('h4'))[0].innerHTML = mainData[5];
+    Array.from(sectionC.getElementsByTagName('p'))[0].innerHTML = mainData[6];
+  
+    Array.from(sectionD.getElementsByTagName('h4'))[0].innerHTML = mainData[7];
+    Array.from(sectionD.getElementsByTagName('p'))[0].innerHTML = mainData[8];
+  
+    Array.from(sectionE.getElementsByTagName('h4'))[0].innerHTML = mainData[9];
+    Array.from(sectionE.getElementsByTagName('p'))[0].innerHTML = mainData[10];
+
+  }
+
+}
+
+processMainContent();
+
+function processContacts() {
+
+  const contactData = [];
+
+  for (var data in siteData[3])
+    contactData.push(siteData[3][data]);
+
+  const contactSection = Array.from(document.getElementsByClassName('contact'))[0];
+
+  contactSection.style.width = "20%";
+
+  Array.from(contactSection.getElementsByTagName('h4'))[0].innerHTML = contactData[0];
+
+  Array.from(contactSection.getElementsByTagName('p'))[0].innerHTML = contactData[1];
+  Array.from(contactSection.getElementsByTagName('p'))[1].innerHTML = contactData[2];
+  Array.from(contactSection.getElementsByTagName('p'))[2].innerHTML = contactData[3];
+
+}
+
+processContacts();
+
+Array.from(Array.from(document.getElementsByTagName('footer'))[0].getElementsByTagName('p'))[0].innerHTML = siteData[4].copyright;
